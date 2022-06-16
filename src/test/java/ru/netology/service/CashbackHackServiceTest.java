9cfac7982fb413028c9c2b58590fdf3c623a7b5a
IlyaMahnach <ilya_mahnach@mail.ru>
2022-06-16 16:00:48 +0700

    junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,37 +1,37 @@
 package ru.netology.service;
 
 
-import static org.testng.AssertJUnit.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void remainTest1() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
         int expected = 100;
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
         System.out.println("Dlya polucheniya bonusa dokupitye na " + service.remain(amount) + " rubley");
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void remainTest2() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
-        int expected = 1000;
+        int expected = 0;
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
         System.out.println("Dlya polucheniya bonusa dokupitye na " + service.remain(amount) + " rubley");
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void remainTest3() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1500;
         int expected = 500;
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
         System.out.println("Dlya polucheniya bonusa dokupitye na " + service.remain(amount) + " rubley");
     }
 }
