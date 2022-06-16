9a01cc7abc0acf65333988fc02536624a53861aa
IlyaMahnach <98087178+IlyaMahnach@users.noreply.github.com>
2022-06-16 16:20:59 +0700

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,10 +7,30 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashbackHackServiceTest {
 
     @Test
-    void remainTest() {
+    void remainTest1() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
         int expected = 100;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+        System.out.println("Dlya polucheniya bonusa dokupitye na " + service.remain(amount) + " rubley");
+    }
+    
+     @Test
+    void remainTest2() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+        System.out.println("Dlya polucheniya bonusa dokupitye na " + service.remain(amount) + " rubley");
+    }
+    
+     @Test
+    void remainTest3() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1500;
+        int expected = 500;
         int actual = service.remain(amount);
         assertEquals(expected, actual);
         System.out.println("Dlya polucheniya bonusa dokupitye na " + service.remain(amount) + " rubley");
